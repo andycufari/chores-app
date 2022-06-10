5975dcd9bfa66cebe8c4f4049c4248985d1ab223
Martin de Amorin <martindeamorin@gmail.com>
2022-06-10 19:17:13 -0300

    funcionalidad basica cambio de pestaña

--- App/navigation/TeamStack.js
+++ App/navigation/TeamStack.js
@@ -1,16 +1,45 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import TeamScreen from '../screens/TeamScreen';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Icon } from 'react-native-elements';
+import TestScreen from '../screens/TestScreen';
 
 const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        tab name="Feed" 
+        component={TeamScreen} 
+        options={{
+          tabBarLabel: "Registro",
+          tabBarIcon: () => {
+            return <Icon type="material-community" name="logout" size={26}/>
+          }
+        }}
+        />
+      <Tab.Screen 
+      name="Messages"
+       component={TestScreen}
+       options={{
+        tabBarLabel: "Estadisticas",
+        tabBarIcon: () => {
+          return <Icon type="material-community" name="logout" size={26}/>
+        }
+      }} />
+    </Tab.Navigator>
+  );
+}
 export const TeamStack = () => {
   return (
     <Stack.Navigator
       initialRouteName='Team'
       screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name='Team' component={TeamScreen} />
+      <Stack.Screen name='Team' component={Home} />
     </Stack.Navigator>
   );
 };
---
+++ App/screens/TestScreen.js
@@ -0,0 +1,5 @@
+export default function TestScreen(){
+    return(
+        <h1>TestStack</h1>
+    )
+}
